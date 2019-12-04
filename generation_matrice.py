7e68b9490b90a7147996c9500ee5e51a76c04d28
Aurel37 <pion.aurelien@outlook.com>
2019-12-04 18:35:06 +0100

    gestion vaccin alea

--- generation_matrice.py
+++ generation_matrice.py
@@ -1,6 +1,15 @@
 from random import uniform
 import numpy as np
 
+def gen_vect(n_pop, p_sick):
+    X = []
+    for i in range(n_pop):
+        if (uniform(0,1)<p_sick):
+            X.append(True)
+        else:
+            X.append(False)
+    return X
+
 def gen_matrix(n,W):
     """
     n in an integer that represent the number of edges of the graph
@@ -39,6 +48,19 @@ def transition(matrix, X, p_heal, p_infect, heals):
                 if (count_voisin >0 and uniform(0,1)<(p_infect*poids)/count_voisin):
                             X[i] = True
                             print('yes')
+
+def vaccin_rand(X, m, matrix, mu_vaccin):
+    n = len(X)
+    l_n = [i for i in range(n)]
+    shuffle(l_n)
+    for i in l_n:
+        if (uniform(0,1) < mu_vaccin):
+            X[i] = False
+            for p in range(len(matrix)):
+                for k in range(len(matrix)):
+                    if (matrix[p][k]>0):
+                        matrix[p][k] = 0;
+
 def w(x,y):
     return (x+y)/2
 
@@ -47,6 +69,8 @@ G = gen_matrix(5,w)
 X = [True, False, False, True, True]
 heals = [0, 0, 0, 0, 0]
 
-transition(G, X, 0.5, 0.7, heals)
+vaccin_rand(X, 2, G, 0.08)
+print(X)
+print(G)
 
 print(X)
