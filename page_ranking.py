d8fc401619e11d5f9c24376e295def7347a12a49
Aurel37 <pion.aurelien@outlook.com>
2020-01-22 23:41:49 +0100

    correction of the page_ranking process

--- page_ranking.py
+++ page_ranking.py
@@ -18,38 +18,41 @@ def copie_vect(u):
 
 
 def norme(u):
-    test = 0
     res = 0
     n = len(u)
     for i in range(n):
         res += u[i]**2
     return res**(1/2)
 
-def somme_ligne_i(A,i):
+
+def somme_colonne_i(A, i):
     res = 0
     n = len(A)
     for k in range(n):
-        res += A[i,k]
+        res += A[k, i]
     return res
+
 
 def normalisation(A):
     n = len(A)
-    RES = np.zeros((n,n))
+    RES = np.zeros((n, n))
     for i in range(n):
-        somme = somme_ligne_i(A,i)
         for j in range(n):
-            if (somme != 0):
-                RES[i,j] = A[i,j]/somme
+            somme = somme_colonne_i(A, j)
+            if somme != 0:
+                RES[i, j] = A[i, j]/somme
     return RES
 
-def page_ranking(A, m, X, seuil):
+
+def page_ranking(Weight_mat, m, X, seuil):
     n = len(A)
     u0 = [1 for i in range(n)]
     u1 = [0 for i in range(n)]
-    B = normalisation(A)
+    Weight_mat_n = normalisation(Weight_mat)
     while (norme(soustraction(u0, u1)) > seuil):
         u1 = copie_vect(u0)
-        u0 = np.dot(B, u0)
+        print(Weight_mat_n)
+        u0 = np.dot(Weight_mat_n, u0)
         print('inside', u0)
         u0 = u0/norme(u0)
     indice = []
@@ -57,18 +60,17 @@ def page_ranking(A, m, X, seuil):
     print("u0_end", u0)
     for i in range(n):
         propre_indice.append((abs(u0[i]), i))
-    propre_indice = sorted(propre_indice, key = lambda colonnes: colonnes[0])
-    for i in range(m):
+    propre_indice = sorted(propre_indice, key=lambda colonnes: colonnes[0])
+    for i in range(n-1, n-m-1, -1):
         indice = propre_indice[i][1]
         X[indice] = True
         for j in range(n):
             A[indice][j] = 0
-            A[i][indice] = 0
+            A[j][indice] = 0
 
-A = gen_matrix(5, w)
-X = [True, False, True, False, False]
+
+A = gen_matrix(10, w)
+X = [False for i in range(10)]
 print(A)
-page_ranking(A, 2, X, 0.01)
+page_ranking(A, 2, X, 0.1)
 print(A)
-
-
